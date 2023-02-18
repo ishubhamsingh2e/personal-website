26153b3a1285d8813bb7a7b29558fc085b624cf2
Shubham Singh <ishubhamsingh2e@gmail.com>
2023-02-18 09:24:32 +0530

    removed: sitemap

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from .feed import BlogsFeed
-from django.contrib.sitemaps.views import sitemap
-from .sitemap import BlogSiteMap
 from . import views
-
-sitemaps = {'blog': BlogSiteMap, }
 
 urlpatterns = [
     path('', views.blog, name='blog'),
@@ -14,7 +10,5 @@ urlpatterns = [
         name='article'
     ),
     path('rss/', BlogsFeed(), name='rss'),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap'),
     path('category/<_filter>/', views.category, name='category'),
 ]
