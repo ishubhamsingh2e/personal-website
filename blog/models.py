e02f4de6e9dab50f0367e08fc7b285b4028558a2
Shubham Singh <ishubhamsingh2e@gmail.com>
2023-02-18 21:29:46 +0530

    fix: id not populating

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,12 @@ class Blog(models.Model):
         ('draft', 'draft'),
         ('published', 'published'),
     }
-
+    id = models.BigAutoField(
+        auto_created=True,
+        primary_key=True,
+        serialize=False,
+        verbose_name='ID'
+    )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     date = models.DateField(auto_now_add=True)
@@ -29,7 +34,6 @@ class Blog(models.Model):
     hero_image = models.URLField()
     hero_image_alt = models.CharField(max_length=300, blank=True)
     meta_description = models.TextField()
-    quote = models.TextField()
     body = models.TextField()
     status = models.CharField(max_length=10, choices=STATUS_CHOICE,
                               default='draft')
@@ -47,5 +51,3 @@ class Blog(models.Model):
 
     def __str__(self):
         return f"{self.id}. {self.title}"
-
-# Create your models here.
